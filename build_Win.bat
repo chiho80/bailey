9712c01ab6a129d61d61eb45294644daa6fd8bde
chiho80 <chiho80@gmail.com>
2024-05-15 15:39:01 -0700

    Updated builders

--- build_Win.bat
+++ build_Win.bat
@@ -1,8 +1,5 @@
 .venv\Scripts\activate
 pyinstaller -y main.spec
-rm -rf scripts/__pycache__
-cp -rf scripts dist/Bailey.app/Contents/Frameworks/
-cp -rf data dist/Bailey.app/Contents/Frameworks/
-cp -rf scripts dist/main/_internal
-cp -rf data dist/main/_internal
-rm -rf build
+copy scripts dist/main/_internal
+copy data dist/main/_internal
+rd build
